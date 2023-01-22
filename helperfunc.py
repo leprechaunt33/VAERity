a29e39ac2748ab8e78cb26de83396d5fcd8b7c1d
leprechaunt33 <121342696+leprechaunt33@users.noreply.github.com>
2023-01-23 00:44:15 +1100

    - Added code overriding global file path to ensure data files are loaded from vaersfolder. and not prior hardcoded location.

--- helperfunc.py
+++ helperfunc.py
@@ -22,6 +22,8 @@ MAX_AGE=120
 TOTALBINS=int(MAX_AGE/AGE_BINS)+1
 
 def vaershandle(what: str = 'DATA', years: object = 'all') -> vx.DataFrame:
+    currapp=kivy.app.App.get_running_app()
+    dbfilepath = currapp._vr['vaersfolder']
     what = str(what).upper()
     if type(years) == str:
         if years == 'all':
