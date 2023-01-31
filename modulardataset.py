969f453cb07f8f7fd91bc92b555dec6a1c3a27b5
leprechaunt33 <121342696+leprechaunt33@users.noreply.github.com>
2023-01-31 14:03:40 +1100

    - Base dataclasses MDField and MDDataTable implemented to describe the database tables and provide search functionality for fields. - dsid is a string ('dataset id') in conjunction with module, added to ModularDataset, along with the datatables dictionary, to accomplish this. - The program looks for a file in the datasets folder with a name ending in .xml corresponding to the dsid of the dataset, oassed during initialisation.  If such an xml file exists, it is parsed by a sax parser (the DatasetParser class) - Currently the only functionality this internal representation of the dataset performs in the program is to return names of fields and their descriptions for display on screen to users during selecting outcomes for example.  However, the structural description of the database may be of use in a future improvement, for instance in ensuring data purity and whether the formal specification is being adhered to in the existing field data.

--- modulardataset.py
+++ modulardataset.py
@@ -1,4 +1,8 @@
+import datetime
+from dataclasses import dataclass, field
 import math, re
+import os.path
+import xml.sax
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
@@ -8,7 +12,44 @@ from kivy.uix.textinput import TextInput
 from kivy.utils import get_color_from_hex
 from dataframegridview import ColoredButton, ColoredLabel
 from kivy.app import App
-#from rootwindow import RootWindow
+
+@dataclass
+class MDField:
+    """Class for describing the fields of the datasets that VAERity manages"""
+    name: str
+    type: type
+    iskey: bool
+    allowna: bool
+    description: str
+    groups: list[str] = field(default_factory=list)
+    dateformat: str = ''
+    maxlength: int = -1
+
+@dataclass
+class MDDataTable:
+    name: str
+    multiplefiles: bool = False
+    fields: list[MDField] = field(default_factory=list)
+    groups: dict = field(default_factory=dict)
+
+    def get_group(self, group: str) -> list[MDField]:
+        if group in self.groups:
+            return self.groups[group]
+        else:
+            return None
+
+    def add_to_group(self, group, field):
+        if group not in self.groups:
+            self.groups[group]=list()
+
+        if field not in self.groups[group]:
+            self.groups[group].append(field)
+
+    def add_field(self, field: MDField):
+        self.fields.append(field)
+        print(f"Adding {field} to groups")
+        for group in field.groups:
+            self.add_to_group(group, field)
 
 class ModularDataset:
     rf = []
@@ -19,8 +60,10 @@ class ModularDataset:
     ref_resolvers: dict = None
     ref_funcs: dict = None
     field_order: dict = None
+    dsid: str = None
+    module: str = None
 
-    def __init__(self, origin, record_format):
+    def __init__(self, origin, record_format, dsid):
         self.currapp=App.get_running_app()
         self.stylekeys = dict()
         self.ids = dict()
@@ -31,12 +74,36 @@ class ModularDataset:
         self.ref_resolvers= dict()
         self.ref_funcs=dict()
         self.orig_cls = origin
+        self.dsid = dsid
+        self.datatables = dict()
         if record_format is None:
             raise ValueError("Please supply the record format")
 
         if isinstance(record_format, list):
             self.rf = record_format
             self.content=self.generate_content()
+
+        datasets=os.path.join(os.path.dirname(__file__), 'datasets')
+        self.xmlfile=os.path.join(datasets, f"{self.dsid}.xml")
+        if os.path.exists(self.xmlfile):
+            self.mdparser=DatasetParser(self)
+            xml.sax.parse(self.xmlfile, self.mdparser)
+
+    def find_field_by_description(self, description):
+        for table in self.datatables.values():
+            results = [f for f in table.fields if f.description == description]
+            if len(results) > 0:
+                return (table.name, results[0])
+
+        return None
+
+    def find_field(self, name):
+        for table in self.datatables.values():
+            results=[f for f in table.fields if f.name == name]
+            if len(results) > 0:
+                return (table.name, results[0])
+
+        return None
 
     def set_stylekey(self, element, key):
         if key is None:
@@ -311,3 +378,143 @@ class ModularDataset:
                         self.resolvers[item['id']]=item.get('resolver','')
                         self.ref_resolvers[item['id']] = item.get('ref_resolver', '')
         return rc
+
+class DatasetParser(xml.sax.ContentHandler):
+    currenttag: str = None
+    def __init__(self, md: ModularDataset):
+        self.md = md
+        self.currenttag = ''
+        self.seenmodular = False
+
+    def handle_field(self, tag, attributes):
+        if 'name' in attributes:
+            name=attributes['name']
+        else:
+            raise ValueError("A field must have a name!")
+
+        dateformat=''
+
+        if 'type' not in attributes:
+            raise ValueError(f"A field ({name}) must have a type!")
+        else:
+            t=attributes['type']
+            if t == 'char':
+                ourtype=str
+            elif t == 'int':
+                ourtype=int
+            elif t == 'float':
+                ourtype=float
+            elif t == 'string':
+                ourtype = str
+            elif t == 'date':
+                ourtype=datetime.date
+                if 'dateformat' in attributes:
+                    dateformat=attributes['dateformat']
+                else:
+                    raise ValueError(f"If a field ({name}) is a date, it must have a dateformat attribute!")
+            else:
+                raise ValueError(f"Type for {name} must be one of ['char', 'int', 'float', 'string', 'date']")
+
+        if 'description' in attributes:
+            description = attributes['description']
+        else:
+            description = name
+
+        if 'allowna' in attributes:
+            try:
+                allowna=bool(attributes['allowna'])
+            except Exception:
+                print(f"WARNING: Could not parse allowna for field {name}, defaulting to True")
+                allowna=True
+        else:
+            allowna=True
+
+        if 'maxlength' in attributes:
+            try:
+                maxlength = int(attributes['maxlength'])
+            except Exception:
+                print(f"WARNING: Could not parse maxlength for field {name}, defaulting to -1")
+                maxlength=-1
+        else:
+            maxlength=-1
+
+        if 'key' in attributes:
+            try:
+                iskey=bool(attributes['key'])
+            except Exception:
+                print(f"WARNING: Could not parse 'key' for field {name}, defaulting to False")
+                iskey=False
+        else:
+            iskey=False
+
+        if 'groups' in attributes:
+            try:
+                groupnames=str(attributes['groups']).split()
+            except Exception:
+                print(f"WARNING: Could not parse 'groups' for field {name}, groups will not be set!")
+                groupnames=[]
+        else:
+            groupnames=[]
+
+        element=MDField(name=name, description=description, allowna=allowna,
+                        maxlength=maxlength, iskey=iskey, type=ourtype, dateformat=dateformat)
+        for group in groupnames:
+            element.groups.append(group)
+
+        if isinstance(self.currentElement, MDDataTable):
+            self.currentElement.add_field(element)
+        else:
+            print(f"CurrentElement is {type(self.currentElement)}")
+
+    def handle_datatable(self, tag, attributes):
+        if 'name' not in attributes:
+            raise ValueError("A DataTable MUST have a name!")
+        else:
+            name = str(attributes['name'])
+
+        if 'multiplefiles' in attributes:
+            try:
+                multiplefiles = bool(attributes['multiplefiles'])
+            except Exception:
+                print("WARNING: multiplefiles must be True or False.  Defaulting to True")
+                multiplefiles=False
+
+        self.currentElement=MDDataTable(name=name, multiplefiles=multiplefiles)
+    def startElement(self, tag, attributes: xml.sax.xmlreader.AttributesImpl):
+        print(tag,[f"{k}:{v} " for k, v in attributes.items()])
+        if tag == 'ModularDataset':
+            if (self.currenttag == '') and not self.seenmodular:
+                self.seenmodular=True
+                if 'module' in attributes:
+                    self.md.module=attributes['module']
+                if 'identifier' in attributes:
+                    self.md.dsdescription=attributes['identifier']
+                self.currenttag=tag
+            else:
+                print(f"WARNING: Ignoring out of place ModularDataset tag in {self.md.xmlfile}")
+                self.currenttag=tag
+        elif tag == 'DataTable':
+            self.handle_datatable(tag, attributes)
+        elif tag == 'Field':
+            if self.currenttag=='Fields':
+                self.handle_field(tag, attributes)
+            else:
+                print(f"Current tag is {self.currenttag}")
+        elif tag == 'Fields':
+            self.currenttag=tag
+
+    def endElement(self, tag):
+        if tag == 'DataTable':
+            dtable: MDDataTable = self.currentElement
+            if len(dtable.fields) == 0:
+                raise ValueError("A data table must have at least one field!")
+            else:
+                self.md.datatables[dtable.name] = dtable
+            self.currentElement = None
+
+        if tag != 'Field':
+            self.currenttag=''
+
+    def characters(self, content):
+        pass
+
